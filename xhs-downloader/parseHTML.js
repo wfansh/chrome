ec7b258f0c194748d2d823df71f68d6bad88f9a6
Fan Wang <wfansh@google.com>
2023-12-30 16:17:38 +0800

    Update parseHTML.js

--- xhs-downloader/parseHTML.js
+++ xhs-downloader/parseHTML.js
@@ -4,9 +4,8 @@ if (videos.length === 0) {
     alert('No video element found on the current page');
 }
 
-// 2. Sends the runtime message including videoUrl to popup.js to download.
+// 2. Sends the runtime message including videoUrl to 'popup.js' to download.
 var videoUrl = videos[0].getAttribute('src');
-
 chrome.runtime.sendMessage({
     method: 'getVideoUrl',
     url: videoUrl
